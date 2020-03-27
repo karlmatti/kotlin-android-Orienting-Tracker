964371501787feda7f325099f5128de49524eb68
karlmatti <28657926+karlmatti@users.noreply.github.com>
2020-03-27 15:02:52 +0200

    init commit of notification demo

---
+++ app/src/main/java/taltech/karlmatti/sportmap/NotifResultActivity.kt
@@ -0,0 +1,12 @@
+package taltech.karlmatti.sportmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotifResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notif_result)
+    }
+}
