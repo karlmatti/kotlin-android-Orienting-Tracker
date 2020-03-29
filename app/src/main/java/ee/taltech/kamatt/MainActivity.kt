6875d79779dc84c3d7d906f7e813d2a02767e3f4
karlmatti <28657926+karlmatti@users.noreply.github.com>
2020-03-29 14:41:44 +0300

    fixed google maps bug

--- app/src/main/java/ee/taltech/kamatt/MainActivity.kt
+++ app/src/main/java/ee/taltech/kamatt/MainActivity.kt
@@ -20,12 +20,10 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.track_control.*
@@ -296,10 +294,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         Log.d(TAG, "im in onMapReady")
         // Add a marker in Sydney and move the camera
+        /*
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
+        */
     }
 
 }
